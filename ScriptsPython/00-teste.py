850199b17df6c4be950ef88feaee2b1b525af038
Robson Vaamonde <boraparapratica@gmail.com>
2024-10-15 12:31:23 -0300

    Primeiros Scripts em Python

---
+++ ScriptsPython/00-teste.py
@@ -0,0 +1,3 @@
+valor01 = 10
+valor02 = 20
+print(valor01 + valor02)
---
+++ ScriptsPython/03-idade_simples.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+ano_atual = int(input("Digite o ano atual: "))
+ano_nasc = int(input("Digite o Ano do seu nascimento: "))
+idade = int(ano_atual - ano_nasc)
+print()
+print("Sua idade é:", idade,"anos")
